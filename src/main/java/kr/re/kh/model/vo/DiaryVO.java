28c50c2a0749b0c7d0e34f4058007991a98bb057
OriNong <min000922@gmail.com>
2024-11-12 17:43:14 +0900

    수정된 diary 테이블에 맞게 DiaryVO.java 수정 (weatherId -> weatherIcon 변경) 수정된 DiaryVO.java에 맞춰 DiaryMapper.xml 수정 일기 조회 쿼리 * -> 필드 값 나열로 수정

--- src/main/java/kr/re/kh/model/vo/DiaryVO.java
+++ src/main/java/kr/re/kh/model/vo/DiaryVO.java
@@ -14,11 +14,10 @@ public class DiaryVO {
     private Long memberId;                  // 작성자 id
     private String diaryTitle;              // 일기 제목
     private String diaryContent;            // 일기 본문
-    private LocalDateTime diaryCrtDate;     // 일기 생성 일자
-    private LocalDateTime diartyUpdDate;    // 일기 수정 일자
     private boolean isPublic;               // 공개 여부
-    private int weatherId;                  // 날씨 고유 id
     private Long diaryViews;                // 일기 조회 수
     private Long diaryLikes;                // 일기 좋아요 수
     private String weatherIcon;             // 날씨 이모티콘
+    private LocalDateTime diaryCrtDate;     // 일기 생성 일자
+    private LocalDateTime diartyUpdDate;    // 일기 수정 일자
 }
