95ea2626a8308b5e379e11b849131cee515aef4a
OriNong <min000922@gmail.com>
2024-11-12 11:00:24 +0900

    DiraryService.java

--- src/main/java/kr/re/kh/service/DiaryService.java
+++ src/main/java/kr/re/kh/service/DiaryService.java
@@ -1,9 +1,12 @@
 package kr.re.kh.service;
 
 import kr.re.kh.mapper.DiaryMapper;
+import kr.re.kh.model.vo.DiaryVO;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -13,16 +16,33 @@ public class DiaryService {
     private final DiaryMapper diaryMapper;
 
     // 로그인 사용자 자신의 일기 조회
+    public List<DiaryVO> selectAllDiaryByMemberId(Long memberId) {
+        return diaryMapper.selectAllDiaryByMemberId(memberId);
+    }
 
     // 개별 사용자의 공개로 게시된 일기 조회
+    public List<DiaryVO> selectPublicDiaryByMemberId(Long memberId) {
+        return diaryMapper.selectPublicDiaryByMemberId(memberId);
+    }
 
     // 공개로 게시된 전체 일기 조회
-
+    public List<DiaryVO> selectAllPublicDiary() {
+        return diaryMapper.selectAllPublicDiary();
+    }
     // 일기 저장
-
+    public void saveDiary(DiaryVO diary) {
+        diaryMapper.saveDiary(diary);
+    }
     // 일기 수정 시 해당 일기 조회 후 반환
-
+    public DiaryVO selectDiaryWhenUptDiary(Long diaryId) {
+        return diaryMapper.selectDiaryWhenUptDiary(diaryId);
+    }
     // 일기 수정
-
+    public void updateDiary(DiaryVO diary) {
+        diaryMapper.updateDiary(diary);
+    }
     // 일기 삭제
+    public void deleteDiary(Long diaryId) {
+        diaryMapper.deleteDiary(diaryId);
+    }
 }
