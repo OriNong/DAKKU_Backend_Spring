872446d8d4324159ebcdaebf64725a916b1b88a5
lee0802-120 <lpk3782@gmail.com>
2024-11-28 14:23:21 +0900

    채팅방 목록 VO 추가.

--- src/main/java/kr/re/kh/model/vo/ChatListVO.java
+++ src/main/java/kr/re/kh/model/vo/ChatListVO.java
@@ -9,12 +9,16 @@ import lombok.*;
 public class ChatListVO {
     private String roomId;
     private String userName;
+    private Long friendId;
     private String friendName;
+    private boolean me;
 
     @Builder
-    public ChatListVO(String roomId, String userName, String friendName) {
+    public ChatListVO(String roomId, String userName, Long friendId, boolean me, String friendName) {
         this.roomId = roomId;
         this.userName = userName;
+        this.friendId = friendId;
+        this.me = me;
         this.friendName = friendName;
     }
 }
