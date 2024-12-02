a4958aed3029100e76d5aca754069b33e3f9ff5e
choi-GT <kckt3741@gmail.com>
2024-12-02 16:42:29 +0900

    다이어리 전체 조회 websecurityConfig 추가 로그인하지 않아도 조회되기.

--- src/main/java/kr/re/kh/config/WebSecurityConfig.java
+++ src/main/java/kr/re/kh/config/WebSecurityConfig.java
@@ -123,6 +123,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
                 .antMatchers("/**/topic/**").permitAll()
                 .antMatchers("/**/app/**").permitAll()
                 .antMatchers("/**/chat/**").permitAll()
+                .antMatchers("/**/api/diary/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
